f074403a064a9b669836597031eb24308f828917
PhzThao <pthao181125@gmail.com>
2025-05-04 00:58:31 +0700

    change host server

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ app.use(cors());
 
 app.use("/api/auth", authroutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on all interfaces");
+});
